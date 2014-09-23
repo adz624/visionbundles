2a435deba15123a0f7fc35903731729b7e81b5e3
eddie.li <eddie@visionbundles.com>
2014-09-24 00:54:08 +0800

    Add fast assets recipes, include compile assets locally and sync to CDN locally

---
+++ lib/visionbundles/recipes/fast_assets.rb
@@ -0,0 +1,73 @@
+require 'visionbundles/asset_sync'
+
+Capistrano::Configuration.instance(:must_exist).load do
+  set_default(:precompile_env)   { rails_env }
+  set_default(:precompile_cmd)   { "RAILS_ENV=#{precompile_env.to_s.shellescape} #{asset_env} #{rake} assets:precompile" }
+  set_default(:cleanexpired_cmd) { "RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} #{rake} assets:clean_expired" }
+  set_default(:rsync_cmd) { "rsync -av" }
+
+  set_default(:assets_dir) { "public/assets" }
+
+  set_default(:cdn) { nil }
+
+  namespace :deploy do
+    namespace :assets do
+
+      desc "remove manifest file from shared path"
+      task :remove_manifest do
+        run "rm -rf #{shared_path}/#{shared_assets_prefix}/manifest*.json"
+      end
+
+      desc "cleanup local compiled assets"
+      task :cleanup, on_no_matching_servers: :continue  do
+        run_locally "rm -rf #{assets_dir}"
+      end
+
+      desc "Precompile assets locally"
+      task :precompile, only: { primary: true }, on_no_matching_servers: :continue do
+        run_locally "#{precompile_cmd}"
+      end
+
+      desc "Sync manifest on app servers"
+      task :sync_manifest_to_app_servers, only: { primary: true }, on_no_matching_servers: :continue do
+        local_manifest_path = run_locally("ls #{assets_dir}/manifest*")
+        local_manifest_path.strip!
+        # Only sync manifest to app server
+        find_servers(roles: :app, except: { no_release: true }).each do |server|
+          server_with_user = "#{user}@#{server}"
+          run_locally "#{rsync_cmd} #{local_manifest_path} #{server_with_user}:#{shared_path}/assets/#{File.basename(local_manifest_path)}"
+          run_locally "#{rsync_cmd} #{local_manifest_path} #{server_with_user}:#{release_path}/assets_manifest#{File.extname(local_manifest_path)}"
+        end
+      end
+
+      desc "Sync assets to CDN or remote app server"
+      task :sync_assets_files, only: { primary: true }, on_no_matching_servers: :continue do
+        if cdn.nil?
+          # If not using CDN, web should have asset files
+          find_servers(roles: :web, except: { no_release: true }).each do |server|
+            server_with_user = "#{user}@#{server}"
+            run_locally "#{rsync_cmd} ./#{assets_dir}/ #{server_with_user}:#{release_path}/#{assets_dir}/"
+          end
+        else
+          # Use AssetSync Gem to sync assets list to cloud
+          # https://github.com/rumblelabs/asset_sync/blob/master/lib/tasks/asset_sync.rake#L5
+          AssetSync.configure do |config|
+            config.manifest_path = run_locally("ls #{assets_dir}/manifest*").strip!
+            config.assets_prefix = 'assets'
+            config.public_path = './public'
+            
+            cdn.each do |option, value|
+              config.send("#{option}=", value)
+            end
+
+          end
+          AssetSync.sync
+        end
+      end
+    end
+
+    before "deploy:assets:symlink", "deploy:assets:remove_manifest"
+    after "deploy:assets:precompile", "deploy:assets:sync_manifest_to_app_servers",
+      "deploy:assets:sync_assets_files", "deploy:assets:cleanup"
+  end
+end
