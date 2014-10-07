efcc5de0b4b4bec9d81779a14d570509a34989df
eddie.li <eddie@visionbundles.com>
2014-10-08 01:41:11 +0800

    Fix typo

--- lib/visionbundles/recipes/secret.rb
+++ lib/visionbundles/recipes/secret.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   begin
     require 'rails'
     major, minor = Rails.gem_version.split(',').map(&:to_i)
-    if major == 4 && [0, 1].includ?(minor)
+    if major == 4 && [0, 1].include?(minor)
       source, desc = case minor
       when 0 then; ['secret_token.rb', 'config/initializers/secret_token.rb']
       when 1 then; ['secrets.yml', 'config/config/secrets.yml']
