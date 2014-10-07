f16d66331dfde2af0426fa48135996dd5b91cfff
eddie.li <eddie@visionbundles.com>
2014-10-07 20:10:18 +0800

    seperate valid tasks, and add remote server connection test

--- lib/visionbundles/recipes/valid.rb
+++ lib/visionbundles/recipes/valid.rb
@@ -1,22 +1,50 @@
+require 'net/ssh/simple'
+
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     desc "validate settings"
     task :valid do
       # Capistrano initial checker
-      %w(Capfile config/deploy.rb).each do |file|
-        unless File.exists?(file)
-          valid_faild "Not found file #{file}, you should run `capify .` to initial capistrano setting."
-        end
-      end
-      # Security:
-      # => 1. rails app secret key
-      # => 2. is preconfig folder in source control.
+      not_found_files = (capistrano_files = %w(./Capfile ./config/deploy.rb)).map { |file|
+        file unless File.exists?(file)
+      }.compact
 
-      # Remote:
-      # => 1. check if deploy user setup already.
-      # => 2. check database setup already.
-      # => 3. check remote servers have permission to access git server
-      # => 4. check local have permission to access remote servers
+      if not_found_files.empty?
+        valid_pass "Capistrano file exists: #{capistrano_files.join(', ')}"
+      else
+        valid_faild "Not found file #{not_found_files.join(', ')}, you should run `capify .` to initial capistrano setting."
+      end
     end
   end
+
+  namespace :valid do
+    desc 'check if server connection ok'
+    task :server_connection do
+      # Servers connection
+      servers = {}
+      find_servers.each do |server|
+        connection = Net::SSH::Simple.async do
+          begin
+            ssh server.to_s, 'echo "test_connection"', user: user
+          rescue; end
+        end
+        servers[server.to_s] = { connection: connection }
+      end
+
+      servers.each do |host, data|
+        response = servers[host][:connection].value
+        if response.nil?
+          valid_faild "Server: #{host} connection faild. please check firewall, ssh service and make sure public is in right place on your server."
+        else
+          valid_pass "Server: #{host} connection successful."
+        end
+      end
+    end
+
+    desc "Check remote servers have permission to access git server"
+    task :git_deploy_key_checker do
+      # TODO: make sure every web and apps have permission to access git repo.
+    end
+  end
+  after 'deploy:valid', 'valid:server_connection', 'valid:git_deploy_key_checker'
 end
