2cd36e85b843ecbf037ed8ab3114bcde1b5784d8
eddie.li <eddie@visionbundles.com>
2014-10-07 20:09:17 +0800

    Fix semantic for variables

--- lib/visionbundles/recipes/preconfig.rb
+++ lib/visionbundles/recipes/preconfig.rb
@@ -24,18 +24,18 @@ Capistrano::Configuration.instance(:must_exist).load do
     after "deploy:finalize_update", "preconfig:symlink"
 
     desc <<-DESC
-    This task will check preconfig source file exists in local, and destination not in source control.
+      This task will check preconfig source file exists in local, and destination not in source control.
       deploy flow will use `ln` command to make a symlink those config file to release folder,
       however this command will not replace when file exists. so we should make sure those file exists in local
-    and destination not in source control.
-      DESC
+      and destination not in source control.
+    DESC
     task :valid do
       unless File.exists?(preconfig_dir)
         valid_faild "Not found preconfig folder: #{preconfig_dir}"
       end
 
       local_preconfigurations = []
-      remote_preconfigurations = []
+      destination_preconfigurations = []
 
       preconfig_files_list.each do |source, destination|
         local_file_path = "#{preconfig_dir}/#{source}"
@@ -47,15 +47,19 @@ Capistrano::Configuration.instance(:must_exist).load do
 
         # Make sure preconfig destination is out of source control.
         if file_in_source_control?(destination)
-          remote_preconfigurations.push(destination)
+          destination_preconfigurations.push(destination)
         end
       end
 
-      unless local_preconfigurations.empty?
+      if local_preconfigurations.empty?
+        valid_pass "Preconfig source: #{preconfig_files_list.map(&:first).join(', ')}"
+      else
         valid_faild "Not found preconfig files: #{local_preconfigurations.join(', ')}"
       end
 
-      unless remote_preconfigurations.empty?
+      if destination_preconfigurations.empty?
+        valid_pass "Preconfig destination: #{preconfig_files_list.map(&:last).join(', ')}"
+      else
         valid_faild "Have to remove from source control files: #{remote_preconfigurations.join(', ')}"
       end
     end
