521c50d0bda359c6d55e5f33b50a451b9cfebf0c
eddie.li <eddie@visionbundles.com>
2014-10-07 20:09:42 +0800

    Setup secret preconfig path for rails 4.1 and rails 4.0

--- lib/visionbundles/recipes/secret.rb
+++ lib/visionbundles/recipes/secret.rb
@@ -1,3 +1,15 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  preconfig_files('secret_token.rb' => 'config/initializers/secret_token.rb')
+  begin
+    require 'rails'
+    major, minor = Rails.gem_version.split(',').map(&:to_i)
+    if major == 4 && [0, 1].includ?(minor)
+      source, desc = case minor
+      when 0 then; ['secret_token.rb', 'config/initializers/secret_token.rb']
+      when 1 then; ['secrets.yml', 'config/config/secrets.yml']
+      end
+      preconfig_files(source => desc)
+    else
+      raise 'Only support rails 4.1.x and 4.0.x'
+    end
+  rescue; end
 end
