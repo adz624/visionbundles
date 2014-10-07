10aa360c456202fbd51720f3c8a51442b54aab60
eddie.li <eddie@visionbundles.com>
2014-10-07 20:08:36 +0800

    Add comments, remove duplicate valid tasks, because preconfig will check it

--- lib/visionbundles/recipes/db.rb
+++ lib/visionbundles/recipes/db.rb
@@ -1,13 +1,7 @@
 Capistrano::Configuration.instance(:must_exist).load do
   preconfig_files('database.yml' => 'config/database.yml')
 
-  namespace :db do
-    desc "config/database.yml"
-    task :check do
-      if file_in_source_control?('config/database.yml')
-        valid_faild '"config/database.yml should not in source control, please remove and commit, then add line in .gitignore."'
-      end
-    end
-  end
-  after 'deploy:valid', 'db:check'
+  # TODO:
+  # => 1. change the recipes name to :mysql
+  # => 2. check servers can access database?
 end
