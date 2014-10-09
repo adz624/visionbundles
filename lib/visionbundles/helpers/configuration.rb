8213301a9ccea7176ba482a89a2c86f15e7a1966
eddie.li <eddie@visionbundles.com>
2014-10-09 16:29:45 +0800

    Add preconfig option to yaml

--- lib/visionbundles/helpers/configuration.rb
+++ lib/visionbundles/helpers/configuration.rb
@@ -1,14 +1,24 @@
-require 'yaml' # STEP ONE, REQUIRE YAML!
+require 'yaml'
 module Visionbundles
   module Helpers
     module Configuration
-      def config_from_yaml(file_path, env)
-        config = YAML::load_file("./#{file_path}")[env.to_s]
-        config ||= {config: {}, servers: []}
-        config['config'].each do |key, value|
+      def load_config_from(file_path, env)
+        config = YAML::load_file("./#{file_path}.yml")[env.to_s]
+        setup_configs(config['config'] || {})
+        setup_servers(config['servers'] || [])
+        setup_preconfig(config['preconfig' || {}])
+      end
+
+      private
+
+      def setup_configs(configs)
+        configs.each do |key, value|
           send(:set, key, value)
         end
-        config['servers'].each do |server|
+      end
+
+      def setup_servers(servers)
+        servers.each do |server|
           roles = server['roles'].is_a?(Array) ? server['roles'].map(&:to_sym) : [ server['roles'].to_sym ]
           if server['opts'].is_a?(Hash)
             options = Hash[server['opts'].map { |k, y| [k.to_sym, y] }]
@@ -16,6 +26,14 @@ module Visionbundles
           end
           send(:server, server['host'], *roles)
         end
+      end
+
+      def setup_preconfig(preconfig)
+        set :preconfig_dir, preconfig['root'] if preconfig['root'].present?
+        preconfig_mapper = Hash[(preconfig['list'] || []).map { |list|
+          [list['src'], list['dest']]
+        }]
+        preconfig_files Hash[preconfig_mapper] if preconfig_mapper.present?
       end
     end
   end
