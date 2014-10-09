e5ade479e2236a21ed0006f174a8fd57b6b5cad9
eddie.li <eddie@visionbundles.com>
2014-10-09 15:15:53 +0800

    Add more options about puma, and fix some preload app problems

--- lib/visionbundles/recipes/db.rb
+++ lib/visionbundles/recipes/db.rb
@@ -1,6 +1,5 @@
 Capistrano::Configuration.instance(:must_exist).load do
   preconfig_files('database.yml' => 'config/database.yml')
-
   # TODO:
   # => 1. change the recipes name to :mysql
   # => 2. check servers can access database?
--- lib/visionbundles/recipes/puma.rb
+++ lib/visionbundles/recipes/puma.rb
@@ -6,7 +6,10 @@ Capistrano::Configuration.instance(:must_exist).load do
   set_default(:puma_thread_max, 16)
   set_default(:puma_workers, 0)
   set_default(:puma_config_template) { "../templates/puma/config.erb" }
-  set_default(:puma_preload_app) { true }
+  set_default(:puma_preload_app) { false }
+  set_default(:puma_prune_bundler) { false }
+  set_default(:puma_worker_timeout) { nil }
+  set_default(:puma_on_boot_connection_to_activerecord) { nil }
 
   namespace :puma do
     desc "Setup Puma Scripts"
@@ -16,15 +19,9 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
     after 'deploy:setup', 'puma:setup'
 
-    %w[start stop].each do |command|
+    %w[start stop restart stats status phased-restart].each do |command|
       desc "#{command} puma server"
       task command, roles: :app do
-        run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec pumactl -F #{shared_path}/puma/config.rb #{command}"
-      end
-    end
-    desc "restart puma server"
-    task "restart", roles: :app do
-      %w(stop start).each do |command|
         run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec pumactl -F #{shared_path}/puma/config.rb #{command}"
       end
     end
