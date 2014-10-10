9ced199a3c95e057fd89e6b648d567efdd1ebc31
eddie.li <eddie@visionbundles.com>
2014-10-11 00:58:51 +0800

    Add database authenticate, connection check in db recipe

--- lib/visionbundles/recipes/db.rb
+++ lib/visionbundles/recipes/db.rb
@@ -1,6 +1,26 @@
 Capistrano::Configuration.instance(:must_exist).load do
+  require 'active_record'
+
   preconfig_files('database.yml' => 'config/database.yml')
-  # TODO:
-  # => 1. change the recipes name to :mysql
-  # => 2. check servers can access database?
+
+  namespace :db do
+    desc 'check if each servers (web, app, db) can authenticate and connection the database'
+    task :valid do
+      db = YAML::load_file("./#{preconfig_dir}/database.yml")[rails_env.to_s]
+
+      find_servers(roles: [:web, :app, :db]).map.with_index { |server, num|
+          # delete db server port to local
+        Thread.new {
+          host = server.to_s
+          port = Net::SSH::Gateway.new(host, user).open('127.0.0.1', 3306, 3307 + num)
+          begin
+            ActiveRecord::Base.establish_connection(db.merge(host: '127.0.0.1', port: port)).connection
+            valid_pass "Server: #{host} database authenticate and connection successful."
+          rescue Exception => e
+            valid_faild "Server: #{host} cannot connect to database, please check the database.yml or database firewall."
+          end  
+        }
+      }.each { |task| task.join }
+    end
+  end
 end
