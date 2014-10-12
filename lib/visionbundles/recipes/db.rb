02ad05877564b82b006662224cbcc0c7c0bf120c
eddie.li <eddie@visionbundles.com>
2014-10-13 00:35:56 +0800

    all server should valid db connection

--- lib/visionbundles/recipes/db.rb
+++ lib/visionbundles/recipes/db.rb
@@ -4,12 +4,11 @@ Capistrano::Configuration.instance(:must_exist).load do
   preconfig_files('database.yml' => 'config/database.yml')
 
   namespace :db do
-    desc 'check if each servers (web, app, db) can authenticate and connect to database'
+    desc 'check if each servers can authenticate and connect to database'
     task :valid do
       db = YAML::load_file("./#{preconfig_dir}/database.yml")[rails_env.to_s]
 
-      find_servers(roles: [:web, :app, :db]).map.with_index { |server, num|
-          # delete db server port to local
+      find_servers.map.with_index { |server, num|
         Thread.new {
           host = server.to_s
           port = Net::SSH::Gateway.new(host, user).open('127.0.0.1', 3306, 3307 + num)
@@ -17,7 +16,7 @@ Capistrano::Configuration.instance(:must_exist).load do
             ActiveRecord::Base.establish_connection(db.merge(host: '127.0.0.1', port: port)).connection
             valid_pass "Server: #{host} database authenticate and connection successful."
           rescue Exception => e
-            valid_faild "Server: #{host} cannot connect to database, please check the database.yml or database firewall."
+            valid_faild "Server: #{host} cannot connect to database: #{db['host']}, please check the database.yml or database firewall."
           end  
         }
       }.each { |task| task.join }
