3048ea98e746dba59c7db0c59ab1e2eaeb5dfa5c
eddie <eddie@visionbundles.com>
2014-12-18 16:46:54 +0800

    Fix secret recipe

--- lib/visionbundles/recipes/secret.rb
+++ lib/visionbundles/recipes/secret.rb
@@ -1,14 +1,18 @@
 Capistrano::Configuration.instance(:must_exist).load do
   begin
     require 'rails'
-    major, minor = Rails.gem_version.to_s.split(',').map(&:to_i)
-    if major == 4 && [0, 1].include?(minor)
-      source, desc = case minor
-      when 0 then; ['secret_token.rb', 'config/initializers/secret_token.rb']
-      when 1 then; ['secrets.yml', 'config/secrets.yml']
-      end
-      return preconfig_files(source => desc)
+  rescue e
+    raise 'require rails faild'
+  end
+
+  major, minor = Rails::VERSION::STRING.split('.').map(&:to_i)
+  if major == 4 && [0, 1].include?(minor)
+    source, desc = case minor
+    when 0 then; ['secret_token.rb', 'config/initializers/secret_token.rb']
+    when 1 then; ['secrets.yml', 'config/secrets.yml']
     end
-  rescue; end
-  raise 'Only support rails 4.1.x and 4.0.x'
+    preconfig_files(source => desc)
+  else
+    raise 'Only support rails 4.1.x and 4.0.x'
+  end
 end
