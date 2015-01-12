d057634133bf8f9023d75e0475d8e670384df493
eddie <eddie@visionbundles.com>
2015-01-12 16:39:16 +0800

    Add a puma application service setting

--- lib/visionbundles/recipes/puma.rb
+++ lib/visionbundles/recipes/puma.rb
@@ -6,6 +6,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   set_default(:puma_thread_max, 16)
   set_default(:puma_workers, 0)
   set_default(:puma_config_template) { "../templates/puma/config.erb" }
+  set_default(:service_template) { "../templates/puma/service.erb" }
   set_default(:puma_preload_app) { false }
   set_default(:puma_prune_bundler) { false }
   set_default(:puma_worker_timeout) { nil }
@@ -15,8 +16,19 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :puma do
     desc "Setup Puma Scripts"
     task :setup, roles: :app do
+      # Copy configuration template to remote server
       info '[Puma] copying the config'
       template puma_config_template, "#{shared_path}/puma", "config.rb"
+
+      # Copy service launcher to remote server "init.d/"
+      template service_template, "#{shared_path}/puma", "service"
+      service_remote_path = "#{shared_path}/puma/service"
+      run "chmod +x #{service_remote_path}"
+      sudo "rm -rf /etc/init.d/#{application}"
+      sudo "ln -s #{service_remote_path} /etc/init.d/#{application}"
+
+      # Setup app as a daemon
+      sudo "update-rc.d #{application} defaults"
     end
     after 'deploy:setup', 'puma:setup'
 
